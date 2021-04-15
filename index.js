5204a9dda19401e749461ce8df32145299c441ae
b22 <muraru_bogdan22@yahoo.com>
2021-04-15 10:50:10 +0300

    clean start

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("JavaScript - Dogs App");
+
+if (!localStorage.getItem("name") || !localStorage.getItem("password")) {
+  window.location = "/login.html";
+}
+
+document.getElementById("logout").addEventListener("click", () => {
+  localStorage.removeItem("name");
+  localStorage.removeItem("password");
+  window.location = "/";
+});
