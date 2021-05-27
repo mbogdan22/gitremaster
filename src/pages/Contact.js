d8d9dab0e05a5d0bbab10f838e7e026f08d042f8
b22 <muraru_bogdan22@yahoo.com>
2021-05-28 02:39:49 +0300

    email works

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,30 @@
 import React from "react";
 import { SiFacebook, SiInstagram, SiLinkedin, SiGithub } from "react-icons/si";
+import emailjs from "emailjs-com";
+import { init } from "emailjs-com";
+init("user_mHOqMYiHwSuXex5QdIt9g");
 
 function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_rbyqyy8",
+        "template_txed9ar",
+        e.target,
+        "user_mHOqMYiHwSuXex5QdIt9g"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
   return (
     <div className="Contact">
       <div>
@@ -36,6 +59,54 @@ function Contact() {
         </ul>
       </div>
       <div></div>
+
+      <div className="email-container">
+        <form onSubmit={sendEmail}>
+          <div className="row pt-5 mx-auto">
+            <div className="col-8 form-group mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email Address"
+                name="email"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Subject"
+                name="subject"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <textarea
+                className="form-control"
+                id=""
+                cols="30"
+                rows="8"
+                placeholder="Your message"
+                name="message"
+              ></textarea>
+            </div>
+            <div className="col-8 pt-3 mx-auto">
+              <input
+                type="submit"
+                className="btn btn-info"
+                value="Send Message"
+              ></input>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
